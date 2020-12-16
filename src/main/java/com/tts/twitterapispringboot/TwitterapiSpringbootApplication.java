c962f75f794739a0e259eefc7c181a018b080ba9
cbush2014 <cbush5400@gmail.com>
2020-12-16 18:30:24 -0500

    initial project setup files

---
+++ src/main/java/com/tts/twitterapispringboot/TwitterapiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.tts.twitterapispringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterapiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterapiSpringbootApplication.class, args);
+	}
+
+}
